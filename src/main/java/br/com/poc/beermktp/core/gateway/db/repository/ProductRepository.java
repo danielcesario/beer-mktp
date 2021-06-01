850fd92ad90c8507890b2f7560d9d794b87dff5a
Daniel Cesario <danielatcesario@gmail.com>
2021-06-01 11:33:17 -0300

    product highlights

--- src/main/java/br/com/poc/beermktp/core/gateway/db/repository/ProductRepository.java
+++ src/main/java/br/com/poc/beermktp/core/gateway/db/repository/ProductRepository.java
@@ -3,9 +3,12 @@ package br.com.poc.beermktp.core.gateway.db.repository;
 import br.com.poc.beermktp.core.gateway.db.model.Product;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ProductRepository extends PagingAndSortingRepository<Product, Long> {
@@ -14,4 +17,8 @@ public interface ProductRepository extends PagingAndSortingRepository<Product, L
 
     @RestResource(path = "getByCategory", rel = "getByCategory")
     Page<Product> findAllByCategoryCode(String categoryCode, Pageable pageable);
+
+    @RestResource(path = "getProductHighlights", rel = "getProductHighlights")
+    @Query(nativeQuery = true, value = "SELECT * FROM T_PRODUCT order by RAND() limit 4")
+    List<Product> getByRandom();
 }
